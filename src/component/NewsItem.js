ce0fc7a200a60d52de4dbee8a96d45d1510f6ce9
Darshan Patil <110981147+Darshan-1907@users.noreply.github.com>
2024-12-31 19:17:42 +0530

    Update NewsItem.js

--- src/component/NewsItem.js
+++ src/component/NewsItem.js
@@ -1,24 +1,25 @@
-import React, { Component } from 'react'
+  import React, { Component } from 'react'
 
 
-export class NewsItem extends Component {
+  export class NewsItem extends Component {
 
-  render()
-   {
- let {Title, Description,ImageUrl} = this.props;
-    return (
-      <div className='my-3'>
-        <div className="card" style={{width: "18rem"}}>
-  <img src={ImageUrl} className="card-img-top" alt="..."/>
-  <div className="card-body">
-    <h5 className="card-title">{Title}</h5>
-    <p className="card-text">{Description}</p>
-    <a href="NewsDetail" className="btn btm-sm btn-primary">Read More</a>
+    render()
+    {
+  let {Title, Description,imageUrl,newsurl}  = this.props;
+      return (
+        <div className='my-3'>
+          <div className="card" style={{width: "18rem"}}>
+    <img src={imageUrl} className="card-img-top" alt="..."/>
+    <div className="card-body">
+      <h5 className="card-title">{Title}..</h5>
+      <p className="card-text">{Description}..</p>
+      <a rel="nonreferrer" href={newsurl} target="_blank" className="btn btm-sm btn-dark">Read More</a>
+    </div>
   </div>
-</div>
-      </div>
-    )
+          </div>
+        
+      )
+    }
   }
-}
 
-export default NewsItem
+  export default NewsItem
