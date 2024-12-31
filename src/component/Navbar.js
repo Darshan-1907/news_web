4760c461cfaffe53d1e4a2d11b4044e20bab89eb
Darshan Patil <110981147+Darshan-1907@users.noreply.github.com>
2024-12-31 19:19:30 +0530

    Update Navbar.js

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -7,9 +7,13 @@ export class Navbar extends Component {
   render() {
     return (
      <div>
-                <nav className="navbar navbar-expand-lg bg-body-tertiary">
+                <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container-fluid">
-            <a className="navbar-brand" href="/">News 24/7</a>
+           
+            <a className="navbar-brand" href="/">
+            <img
+                src="https://e7.pngegg.com/pngimages/678/286/png-clipart-news-24-24-7-service-television-channel-news-broadcasting-others-television-blue.png"
+                alt="News 24/7 Logo"style={{ width: "120px", height: "60px", marginRight: "10px",  backgroundColor: "rgba(0, 0, 0, 0.5)" }}/><b style={{ fontSize:"30px",fontWeight:"bold"}}>News 24/7</b></a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
             </button>
@@ -31,9 +35,13 @@ export class Navbar extends Component {
             </div>
          </div>
          </nav>
+
      </div>
+     
     )
+
   }
+  
 }
 
 export default Navbar
